13557551ad041d2e5b717f6237b759a30afd316d
GTeague <developer@ableant.com>
2019-11-08 16:36:29 -0600

    Add globus login / logout support

--- app.py
+++ app.py
@@ -1,12 +1,88 @@
-from flask import Flask
+from flask import Flask, url_for, session, redirect, request
 from datetime import datetime
 import re
+import globus_sdk
 
 app = Flask(__name__)
+
+# Run the app if called via script
+if __name__ == '__main__':
+    app.run()
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+
+@app.route('/globus_login')
+def login():
+    """
+    Login via Globus Auth.
+    May be invoked in one of two scenarios:
+
+      1. Login is starting, no state in Globus Auth yet
+      2. Returning to application during login, already have short-lived
+         code from Globus Auth to exchange for tokens, encoded in a query
+         param
+    """
+    # the redirect URI, as a complete URI (not relative path)
+    redirect_uri = url_for('globus_login', _external=True)
+
+    client = load_app_client()
+    client.oauth2_start_flow(redirect_uri)
+
+    # If there's no "code" query string parameter, we're in this route
+    # starting a Globus Auth login flow.
+    # Redirect out to Globus Auth
+    if 'code' not in request.args:
+        auth_uri = client.oauth2_get_authorize_url()
+        return redirect(auth_uri)
+    # If we do have a "code" param, we're coming back from Globus Auth
+    # and can start the process of exchanging an auth code for a token.
+    else:
+        code = request.args.get('code')
+        tokens = client.oauth2_exchange_code_for_tokens(code)
+
+        # store the resulting tokens in the session
+        session.update(
+            tokens=tokens.by_resource_server,
+            is_authenticated=True
+        )
+        return redirect(url_for('index'))
+
+
+@app.route('/globus_logout')
+def logout():
+    """
+    - Revoke the tokens with Globus Auth.
+    - Destroy the session state.
+    - Redirect the user to the Globus Auth logout page.
+    """
+    client = load_app_client()
+
+    # Revoke the tokens with Globus Auth
+    for token in (token_info['access_token']
+                  for token_info in session['tokens'].values()):
+        client.oauth2_revoke_token(token)
+
+    # Destroy the session state
+    session.clear()
+
+    # the return redirection location to give to Globus AUth
+    redirect_uri = url_for('index', _external=True)
+
+    # build the logout URI with query params
+    # there is no tool to help build this (yet!)
+    globus_logout_url = (
+        'https://auth.globus.org/v2/web/logout' +
+        '?client={}'.format(app.config['PORTAL_CLIENT_ID']) +
+        '&redirect_uri={}'.format(redirect_uri) +
+        '&redirect_name=Globus Example App')
+
+    # Redirect the user to the Globus Auth logout page
+    return redirect(globus_logout_url)
+
 
 @app.route("/hello/<name>")
 def hello_there(name):
@@ -24,3 +100,7 @@ def hello_there(name):
 
     content = "Hello there, " + clean_name + "! It's " + formatted_now
     return content
+
+
+def load_app_client():
+    return globus_sdk.ConfidentialAppAuthClient(app.config['APP_CLIENT_ID'], app.config['APP_CLIENT_SECRET'])
